13b7c8a8b953d9eca5d499dce2f11b3140e0d122
boian123 <boiansc30@gmail.com>
2021-02-26 21:11:30 +0200

    yes

--- client/src/components/Handlers.tsx
+++ client/src/components/Handlers.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { getUsers, addUser, updateUsername, deleteUser } from '../API'
-import User from './User'
+import { addUser } from '../API'
+
 import Register from './Register'
 import Navbar from './Navbar'
 
@@ -11,17 +11,9 @@ export default function Handlers() {
 
   const [users, setUsers] = useState<IUser[]>([])
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
 
   
 
-  const fetchUsers = (): void => {
-    getUsers()
-    .then(({ data: { users } }: IUser[] | any) => setUsers(users))
-    .catch((err: Error) => console.log(err))
-  }
 
  const handleSaveUser = (e: React.FormEvent, formData: IUser): void => {
    e.preventDefault()
@@ -35,41 +27,14 @@ export default function Handlers() {
   .catch((err) => console.log(err))
 }
 
-  const handleUpdateUsername = (user: IUser): void => {
-    updateUsername(user)
-    .then(({ status, data }) => {
-        if ( status !== 200) {
-          throw new Error('Error! User not updated')
-        }
-        setUsers(data.users)
-      })
-      .catch((err) => console.log(err))
-  }
 
-  const handleDeleteUser = (_id: string): void => {
-    deleteUser(_id)
-    .then(({ status, data }) => {
-        if (status !== 200) {
-          throw new Error('Error! User not deleted')
-        }
-        setUsers(data.users)
-      })
-      .catch((err) => console.log(err))
-  }
+
 
 
  
   return (
     <div>
-           <Register saveUser={handleSaveUser} />
-            {users.map((user: IUser) => (
-        <User
-          key={user._id}
-          updateUsername={handleUpdateUsername}
-          deleteUser={handleDeleteUser}
-          user={user}
-        />
-        ))}
+           <Register saveUser={handleSaveUser} />    
         <Navbar/>
     </div>
   
